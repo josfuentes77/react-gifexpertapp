17d1a2df24815db74551f0a7cda523dc600aae83
Jos Fuentes 77 <josfuentes77@gmail.com>
2021-04-20 12:00:46 -0600

    docs agregado

---
+++ docs/precache-manifest.35b9d9e8706c6065bffc3ed92093dcfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1de3da55a24a1e06c08cd7fe7ac05e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b0c3bcbb95f0c9d83ad",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0c3bcbb95f0c9d83ad",
+    "url": "/static/js/main.bc3bd03f.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
